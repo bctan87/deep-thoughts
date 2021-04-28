943f5b4d06762f32077034c4487c800ef9bd604e
Ben Tan <btan87@gmail.com>
2021-04-27 17:40:17 -0700

    21.3.2

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
+const { authMiddleware } = require('./utils/auth');
+
 const express = require('express');
+
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
@@ -10,10 +13,12 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate our Apollo server with the Express application as middleware
